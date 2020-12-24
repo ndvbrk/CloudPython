2eb4a2bb83720839de11c61ee8226cc076201f63
ndvbrk <ndvbrk@gmail.com>
2020-12-24 22:17:37 +0200

    added form for code execution

--- server/frontend/src/forms.py
+++ server/frontend/src/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField, PasswordField, HiddenField
+from wtforms import StringField, TextField, SubmitField, PasswordField, HiddenField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -37,4 +37,21 @@ class SignupForm(FlaskForm):
 class ConfirmEmail(FlaskForm):
     """Email Confirmation form."""
     token = HiddenField('token')
+    submit = SubmitField('Submit')
+
+
+class ExecuteForm(FlaskForm):
+    """Execute code please."""
+    email = StringField('Email', [
+        Email(message='Not a valid email address.'),
+        DataRequired()])
+    password = PasswordField('Password', [
+        DataRequired(message="Please enter a password."),
+        Length(min=4, message=('Password is too short')),
+    ])
+    body = TextField('Message', [
+        DataRequired(),
+        Length(min=4, message=('Your message is too short.'))])
+    totp = IntegerField('totp', [DataRequired()])
+    # recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
